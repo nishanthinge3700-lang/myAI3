15b630581070ac955f7fa1cfb8711ea7910222c5
rahilgosalia77-hue <rahilgosalia77@gmail.com>
2025-11-25 23:01:37 +0530

    Update config.ts

--- config.ts
+++ config.ts
@@ -24,7 +24,7 @@ export const DATE_AND_TIME = getDateAndTime();
 export const AI_NAME = "MyAI3";
 export const OWNER_NAME = "Rahil Gosalia";
 
-export const WELCOME_MESSAGE = `Yo Bro, What's up! I'm ${AI_NAME}, an AI assistant created by ${OWNER_NAME}. How can I be of assistant`
+export const WELCOME_MESSAGE = `Yo Bro, What's up! I'm ${AI_NAME}, an AI assistant created by ${OWNER_NAME}. How can I be of assistance`
 
 export const CLEAR_CHAT_TEXT = "New";
 
