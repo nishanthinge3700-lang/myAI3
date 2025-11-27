8e54a710020883367c0acb086a45ff1bba0379ee
nishanthinge3700-lang <nishant.hinge3700@gmail.com>
2025-11-27 18:41:40 +0530

    Update QuickSidebar.tsx

--- app/QuickSidebar.tsx
+++ app/QuickSidebar.tsx
@@ -23,7 +23,7 @@ export default function QuickSidebar() {
         aria-label="Steam Tables"
       >
         <span className="quick-icon" aria-hidden>💧</span>
-        <span className="quick-label">Steam         Tables</span>
+        <span className="quick-label">Steam Tables & Handbook</span>
       </a>
 
       {/* ChemEng Toolbox */}
