5adf362a5dad34f664ce71adbd57ae42406aa841
nishanthinge3700-lang <nishant.hinge3700@gmail.com>
2025-11-27 18:26:23 +0530

    Add files via upload

---
+++ types/data.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+
+export const uploadedDocumentSchema = z.object({
+    id: z.string(),
+    title: z.string(),
+    created_at: z.string(),
+    content: z.string(),
+});
+export type UploadedDocument = z.infer<typeof uploadedDocumentSchema>;
+
+export const chunkSchema = z.object({
+    pre_context: z.string(),
+    text: z.string(),
+    post_context: z.string(),
+    chunk_type: z.enum(["image", "text"]),
+    source_url: z.string(),
+    source_description: z.string(),
+    source_name: z.string(),
+    order: z.number(),
+});
+export type Chunk = z.infer<typeof chunkSchema>;
+
+export const sourceSchema = z.object({
+    chunks: z.array(chunkSchema),
+    source_url: z.string(),
+    source_description: z.string(),
+    source_name: z.string(),
+});
+export type Source = z.infer<typeof sourceSchema>;
+
+export const citationSchema = z.object({
+    source_url: z.string(),
+    source_description: z.string(),
+});
+export type Citation = z.infer<typeof citationSchema>;
